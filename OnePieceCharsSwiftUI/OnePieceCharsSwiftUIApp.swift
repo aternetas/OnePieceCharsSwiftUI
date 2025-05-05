c412cb971baae044928ecc24cdc6fd81aa832b8e
kot lena <aternetas@gmail.com>
2025-05-05 15:12:34 +0300

    init

---
+++ OnePieceCharsSwiftUI/OnePieceCharsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePieceCharsSwiftUIApp.swift
+//  OnePieceCharsSwiftUI
+//
+//  Created by f f on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnePieceCharsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
