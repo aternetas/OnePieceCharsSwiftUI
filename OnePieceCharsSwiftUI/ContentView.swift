6f5e9cb8807eb3acc68fff5cfe170c1118370d01
kot lena <aternetas@gmail.com>
2025-05-19 19:44:24 +0300

    change some assets, add icons in the table

--- OnePieceCharsSwiftUI/ContentView.swift
+++ OnePieceCharsSwiftUI/ContentView.swift
@@ -11,12 +11,21 @@ struct ContentView: View {
     var body: some View {
         let chars = CharacterService.shared.characters
         
-        List(chars) {
-            Image($0.icon)
-            Text("\($0.name)")
-            
-            Section {}
+        List(chars) { char in
+            HStack {
+                Image(char.icon)
+                    .resizable()
+                    .frame(width: 130, height: 130)
+                    .clipShape(Circle())
+                
+                Text("\(char.name)")
+                    .font(.system(size: 16, weight: .semibold, design: .rounded))
+                    .bold()
+                    .padding(.leading, 10)
+                
+            }
         }
+        .listStyle(.plain)
         .background(.yellow)
         .scrollContentBackground(.hidden)
     }
